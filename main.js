508abd117549612f5f7e867e4ae9e76ebe7f39bd
Joaquim Escrihuela <xescrihuelc@outlook.com>
2025-02-06 13:11:47 +0100

    Añadida comprobacion del lenght de id passwd del html en main.js

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 /* <8 */
 /* 8-10 */
 /* >10 */
+
+/* function name(params) {
+    
+} */
+
+function contar() {
+    let num = caract.textLength;
+    if (num <= 7) {
+        console.log("menor");
+    } else if (num >= 11) {
+        console.log("mayor");
+    } else {
+        console.log("entre");
+    }
+}
+const caract = document.getElementById("passwd");
+caract.addEventListener("input", contar);
